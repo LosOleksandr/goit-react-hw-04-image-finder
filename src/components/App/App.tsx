23e37bf22a1c0ac516d1e8fd03d2feda50a85d3d
LosOleksandr <losalexander2003@gmail.com>
2023-07-08 12:51:19 +0300

    fix

---
+++ src/components/App/App.tsx
@@ -0,0 +1,63 @@
+import SearchForm from 'components/SearchForm/SearchForm';
+import { useEffect, useState, useCallback } from 'react';
+import { Image } from 'interfaces/Image';
+import fetchImages from 'api/finder-api';
+import ImageGallery from 'components/ImageGallery/ImageGallery';
+import LoadMore from 'components/LoadMore/LoadMore';
+import Loader from 'components/Loader/Loader';
+import { Container } from './App.styled';
+
+export default function App() {
+  const [value, setValue] = useState('');
+  const [images, setImages] = useState<Image[]>([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [isLoadmoreShown, setisLoadmoreShown] = useState(false);
+  const [error, setError] = useState(null);
+
+  const getImages = useCallback(async () => {
+    try {
+      setLoading(true);
+      const { hits, total } = await fetchImages(value, page);
+      if (!hits.length) {
+        throw new Error('No matches!');
+      }
+      setImages(prevImages =>
+        page === 1 ? [...hits] : [...prevImages, ...hits]
+      );
+      setisLoadmoreShown(total / page > 12);
+      setError(null);
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [page, value]);
+
+  useEffect(() => {
+    if (!value) return;
+    setisLoadmoreShown(false);
+    getImages();
+  }, [value, page, getImages]);
+
+  return (
+    <main>
+      <Container>
+        {loading && <Loader />}
+        <SearchForm
+          setValue={setValue}
+          setPage={setPage}
+          setImages={setImages}
+        />
+        {images.length !== 0 && (
+          <>
+            <p>Result for {value}</p>
+            <ImageGallery images={images} />
+          </>
+        )}
+        {error && <h2>{error}</h2>}
+        {isLoadmoreShown && <LoadMore setPage={setPage} />}
+      </Container>
+    </main>
+  );
+}
---
+++ src/components/ImageGallery/ImageGallery.styled.ts
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const GalleryList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  @media (max-width: 1400px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 1180px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  column-gap: 10px;
+  row-gap: 20px;
+  img {
+    display: block;
+    object-fit: cover;
+    width: 100%;
+    height: 280px;
+  }
+`;
